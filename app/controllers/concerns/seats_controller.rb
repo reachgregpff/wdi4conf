85675e5ea4b989b8a0c3dacb0edf63836cdbee41
bono <bono.lau@gmail.com>
2015-12-09 15:06:57 +1100

    with payment button

--- app/controllers/concerns/seats_controller.rb
+++ app/controllers/concerns/seats_controller.rb
@@ -17,7 +17,30 @@ class SeatsController < ApplicationController
     @free_seats_list.shuffle!
     puts @free_seats_list
 
-
   end
 
+
+
+  
+  def create
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+
+    rescue Stripe::CardError => e
+     flash[:error] = e.message
+     redirect_to new_charge_path
+  end
+  
 end
